da1b90b5c255a07cab4f42b78408db8990c6ce36
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2019-10-04 08:15:12 +0200

    cambios

---
+++ Tema1/matrices.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MATRICES</title>
+</head>
+<body>
+    <?php
+        $matriz1=[
+            [0,1,2,3],
+            [1,4,3,1]
+        ];
+
+        $matriz2=[
+            [4,0,3],
+            [1,0,1],
+            [2,1,4],
+            [3,2,2]
+        ];
+
+
+
+
+
+    ?>
+</body>
+</html>
