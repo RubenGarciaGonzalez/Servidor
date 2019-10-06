089cf23e9b6ff0782a3769623925b9b840aa9800
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2019-10-06 11:37:24 +0200

    siete

---
+++ Tema1/Relacion/siete.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    </head>
+    <title>Relación de cadenas en PHP nº1</title>
+</head>
+<body style="background-color:#5d99c6"> 
+    <div class="container, mt-4">
+        <h3 class="text-center">Ejercicio 7</h3>
+        <?php
+            $cadena="CadenaOriginal";
+            echo "La cadena que se va a repetir es $cadena<br>";
+        ?>
+    </div>
+    </body>
+</html>
