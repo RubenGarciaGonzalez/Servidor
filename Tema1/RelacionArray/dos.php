c0e2014e3a1eb0d1e1946bc99c697e31da582250
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2019-10-07 12:24:33 +0200

    dos

--- Tema1/RelacionArray/dos.php
+++ Tema1/RelacionArray/dos.php
@@ -11,8 +11,24 @@
     <div class="container, mt-4">
         <h3 class="text-center">Ejercicio 2</h3>
         <?php
-           
+           /*
+           Imprime los valores del array asociativo siguiente usando la estructura de control foreach:
+            $v[1]=90;
+            $v[30]=7;
+            $v['e']=99;
+            $v['hola']=43;
+           */
           
+           $array=[
+               1=>90,
+               30=>7,
+               "e"=>99,
+               "hola"=>43
+           ];
+
+           foreach ($array as $k => $v) {
+            echo "\$array[$k]=".$v."<br>";
+           }   
         ?>
     </div>
     </body>
